41a79b28e7b1f786e28cd1d4b54858c9a8961414
hanlei99977 <1321617737@qq.com>
2026-06-26 18:28:49 +0800

    暂时删除军费支出

--- assets/script/config/RecruitConfig.ts
+++ assets/script/config/RecruitConfig.ts
@@ -6,5 +6,5 @@ export class RecruitConfig {
     static readonly TIME = 10;// 征兵时间
     static readonly MAX_QUEUE_PER_NODE = 5;// 最大征兵队列
 
-    static readonly MILITARY_GOLD_COST_RATE = 0.05;// 军事花费比率
+    static readonly MILITARY_GOLD_COST_RATE = 0.00;// 军事花费比率 暂修改为无军费
 }
